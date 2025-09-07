0a1dcdef09d2b98bf55798838e2c65377cf164a5
Victor Santos <menezes.victorsantos@outlook.com>
2025-09-07 06:30:41 -0300

    Update ddl.sql

--- data/ddl.sql
+++ data/ddl.sql
@@ -1,24 +1,25 @@
--- DISCLAIMER: THIS DDL FILE IS IN BETA AND WILL BE CHANGED
+-- !! DDL NOT FINISHED YET !!
 -- Init
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE DATABASE DEV_ALPHABET;
 
--- /////////////////////////////////////// --
+-- //////////////// Creating tables //////////////// --
 
--- Creating tables
+-- ROLE
 CREATE TABLE IF NOT EXISTS tb_role(
 	id			SERIAL PRIMARY KEY,
     name		VARCHAR(16) NOT NULL,
     isValid		BOOLEAN DEFAULT TRUE
 );
 
+-- PERMISSION
 CREATE TABLE IF NOT EXISTS tb_permission(
 	id			SERIAL PRIMARY KEY,
     name		VARCHAR(48) NOT NULL UNIQUE,
     isValid		BOOLEAN DEFAULT TRUE
 );
 
-
+-- ROLE_PERMISSION
 CREATE TABLE IF NOT EXISTS tb_role_permission(
 	id 				SERIAL PRIMARY KEY,
     roleId			INT NOT NULL,
@@ -27,6 +28,7 @@ CREATE TABLE IF NOT EXISTS tb_role_permission(
     FOREIGN KEY (permissionId) REFERENCES tb_permission(id)
 );
 
+-- PROVIDER
 CREATE TABLE IF NOT EXISTS tb_provider(
 	id			SERIAL PRIMARY KEY,
     name		VARCHAR(32) NOT NULL,
@@ -35,6 +37,7 @@ CREATE TABLE IF NOT EXISTS tb_provider(
     updatedAt	TIMESTAMP NOT NULL DEFAULT now()
 );
 
+-- USER
 CREATE TABLE IF NOT EXISTS tb_user(
 	id			UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name		VARCHAR(20) NOT NULL,
@@ -50,9 +53,8 @@ CREATE TABLE IF NOT EXISTS tb_user(
     FOREIGN KEY (providerId) REFERENCES tb_provider(id)
 );
 
-
-
-CREATE TABLE IF NOT EXISTS tb_session(
+-- SESSION
+CREATE TABLE IF NOT EXISTS tb_user_session(
 	id				UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 	userId			UUID NOT NULL,
 	ipAdress		VARCHAR(16) NOT NULL,
@@ -60,9 +62,9 @@ CREATE TABLE IF NOT EXISTS tb_session(
 	FOREIGN KEY (userId) REFERENCES tb_user(id)
 );
 
--- ////////////////////////////////////// --
+-- ///////////////// Defining functions and triggers ///////////////// --
 
--- Defining functions and triggers
+-- Function for update date
 CREATE OR REPLACE FUNCTION updatedAt()
 	RETURNS TRIGGER AS $$
 BEGIN
@@ -71,6 +73,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+--Its triggers
 CREATE TRIGGER user_info_updated
 BEFORE UPDATE ON tb_user
 FOR EACH ROW
@@ -81,100 +84,25 @@ BEFORE UPDATE ON tb_provider
 FOR EACH ROW
 EXECUTE FUNCTION updatedAt();
 
-
+-- Function to verify user's active sessions
 CREATE OR REPLACE FUNCTION verifyUserActiveSessions()
 	RETURNS TRIGGER AS $$
 DECLARE 
-	user_sessions INTEGER;	
+	ip_sessions INTEGER;	
 	max_sessions  INTEGER;
 BEGIN
-	SELECT COUNT(*) INTO user_sessions FROM tb_session WHERE userId = NEW.userId AND ipAdress = NEW.ipAdress;
-	SELECT COUNT(*) INTO max_sessions FROM tb_session WHERE userId = NEW.userId;
-	IF user_sessions >= 2 OR max_sessions >= 6 THEN
+	SELECT COUNT(*) INTO ip_sessions FROM tb_user_session WHERE userId = NEW.userId AND ipAdress = NEW.ipAdress;
+	SELECT COUNT(*) INTO max_sessions FROM tb_user_session WHERE userId = NEW.userId;
+	IF ip_sessions >= 2 OR max_sessions >= 6 THEN
 		RAISE EXCEPTION 'User sessions limit exceeded.';
 	END IF;
 	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
+-- Its trigger
 CREATE TRIGGER verify_user_sessions
-BEFORE INSERT ON tb_session
+BEFORE INSERT ON tb_user_session
 FOR EACH ROW
 EXECUTE FUNCTION verifyUserActiveSessions();
-
--- /////////////////////////////////////// --
-
--- Populating
-	-- Provider
-	INSERT INTO tb_provider(name)
-		VALUES('Default authentication');
-	INSERT INTO tb_provider(name)
-		VALUES('Apple');
-	INSERT INTO tb_provider(name)
-		VALUES('Google');
-        
--- ////////////////////////////////////// --
-	
-	-- Role
-	INSERT INTO tb_role(name)
-		VALUES('User');
-    INSERT INTO tb_role(name)
-		VALUES('Owner');
-
-	-- Permission
-		-- System Permission
-	INSERT INTO tb_permission(name)
-		VALUES('manage:system');
-		
-		-- Resume Permission
-	INSERT INTO tb_permission(name)
-		VALUES('create:resume'),
-			  ('update:resume'),
-			  ('delete:resume'),
-			  ('read:resume');
-		
-		-- User Permission
-	INSERT INTO tb_permission(name)
-		VALUES('logout:user'),
-			  ('update:user'),
-			  ('ban:user');
-
-		-- Templates Permission
-	INSERT INTO tb_permission(name)
-		VALUES('create:template'),
-			  ('read:template'),
-			  ('use:template'),
-			  ('delete:template'),
-			  ('publish:template');
-
-		-- Account Permission
-	INSERT INTO tb_permission(name)
-		VALUES('create:account'),
-			  ('delete:account'),	
-			  ('read:account'),
-			  ('update:account');
-
-		-- Role Permission
-	INSERT INTO tb_permission(name)
-		VALUES('create:role'),
-			  ('read:role'),
-			  ('update:role'),
-			  ('delete:role');
-
-	-- User
-	INSERT INTO  tb_user(name, lastName, email, password, providerId, icon) 
-		VALUES('user', 'test', 'user@test.com', 'testpassword', 1, '/icon.png'),
-			  ('jonh', 'doe', 'jonhdoe@test.com', 'jonhdoepass', 1, '/jonh.png');
-
-	-- Session
-	DO $$
-	DECLARE
-		firstUser UUID;
-	BEGIN 
-		SELECT id INTO firstUser FROM tb_user LIMIT 1;
-		INSERT INTO tb_session(userId, ipAdress)
-		VALUES(firstUser, '192.168.0.1'),
-			  (firstUser, '127.0.0.1');
-	END
-	$$ LANGUAGE plpgsql;
--- DISCLAIMER: THIS DDL FILE IS IN BETA AND WILL BE CHANGED
+-- !! DDL NOT FINISHED YET !!
