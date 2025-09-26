46289ac6786e4a08fd69a1938d2596554a44d661
Victor Santos <menezes.victorsantos@outlook.com>
2025-09-25 21:36:15 -0300

    Stage: OTP verification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,40 +2,54 @@ import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from '../../generated/prisma';
 import { Resend } from 'resend';
-import { openAPI } from 'better-auth/plugins';
+import { emailOTP, openAPI } from 'better-auth/plugins';
 import { ScalarPreferences } from 'src/common/scalar-preferences';
-import generateVerificationEmailLayout from 'src/common/emailVerificationLayout';
+import {
+  generateOTPCodeLayout,
+  generateVerificationEmailLayout,
+} from 'src/common/emailVerificationLayout';
 const prisma = new PrismaClient();
 const resend = new Resend(process.env.RESEND_API_KEY);
 export const auth = betterAuth({
-  plugins: [openAPI(ScalarPreferences)],
+  plugins: [
+    openAPI(ScalarPreferences),
+    emailOTP({
+      async sendVerificationOTP({ email, otp, type }) {
+        const mailSender = resend.emails.send;
+        switch (type) {
+          case 'sign-in':
+            await mailSender({
+              from: 'Resumit <onboarding@resend.dev>',
+              to: [email],
+              subject: 'Sign in into your account',
+              html: generateOTPCodeLayout(otp),
+            });
+          case 'forget-password':
+            await mailSender({
+              from: 'Resumit <onboarding@resend.dev>',
+              to: [email],
+              subject: 'Reset your password',
+              html: generateOTPCodeLayout(otp),
+            });
+          case 'email-verification':
+            await mailSender({
+              from: 'Resumit <onboarding@resend.dev>',
+              to: [email],
+              subject: 'Verify your Resum.it account',
+              html: generateOTPCodeLayout(otp),
+            });
+        }
+      },
+    }),
+  ],
   emailAndPassword: {
     enabled: true,
     requireEmailVerification: true,
-    sendResetPassword: async ({ user, url, token }, request) => {
-      await resend.emails.send({
-        from: 'Resumit <onboarding@resend.dev>',
-        to: [user.email],
-        subject: 'Reset your password',
-        text: `Reset your password in this link: ${url}`,
-      });
-    },
-  },
-
-  emailVerification: {
-    sendVerificationEmail: async ({ user, url, token }, request) => {
-      await resend.emails.send({
-        from: 'Resumit <onboarding@resend.dev>',
-        to: [user.email],
-        subject: 'Verify your email address',
-        html: generateVerificationEmailLayout(url),
-      });
-    },
-  },
-  secret: process.env.BETTER_AUTH_SECRET,
-  session: {
-    cookieCache: {
-      enabled: true,
+    secret: process.env.BETTER_AUTH_SECRET,
+    session: {
+      cookieCache: {
+        enabled: true,
+      },
     },
   },
   trustedOrigins: [process.env.UI_URL as string],
