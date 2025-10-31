24d0b2fa584a1d6b351a2811b13bbff9102f624f
Victor Santos <menezes.victorsantos@outlook.com>
2025-10-31 03:35:52 -0300

    comment: auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaService } from '../database/prisma.service';
 const prisma = new PrismaService();
+// This auth object only exists to make better-auth cli works fine when generating new schemas or migrating them.
 export const auth = betterAuth({
   user: {
     additionalFields: {
